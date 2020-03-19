463683df109c6dafe3a45a7fdd6b1ec6538ec5eb
tavariscodes <tavarisjohnson@Tavariss-MacBook-Air.local>
2020-03-19 16:41:31 -0500

    guess

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use std::io; // Get "io" module from std library 
 use rand::Rng;
-
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
-
-    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
@@ -16,5 +14,14 @@ fn main() {
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse() // shadow guess variable to u32 type
+        .expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
