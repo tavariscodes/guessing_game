cd29c522079f543395a28843a6f3920ca57da49a
tavariscodes <tavarisjohnson@Tavariss-MacBook-Air.local>
2020-03-19 16:47:47 -0500

    loop

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,31 @@ use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
-
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("Please input your guess.");
 
-    let mut guess = String::new();
+    loop {
+    
+        println!("Please input your guess.");
+    
+        let mut guess = String::new();
+    
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        let guess: u32 = guess.trim().parse() // shadow guess variable to u32 type
+            .expect("Please type a number!");
+    
+        println!("You guessed: {}", guess);
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-
-    let guess: u32 = guess.trim().parse() // shadow guess variable to u32 type
-        .expect("Please type a number!");
-
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break
+            }
+        }
     }
+        
 }
